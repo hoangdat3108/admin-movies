3db4145eaae61e99c94ed08879b347391868ffb9
hoangdat <hoangdatpro1998@gmail.com>
2022-09-27 22:36:30 +0700

    ..

--- src/common/components/SideBar/index.jsx
+++ src/common/components/SideBar/index.jsx
@@ -1,22 +1,79 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import style from "./style.module.css";
 import { FaBars } from "react-icons/fa";
-import { AiOutlineClose } from "react-icons/ai";
-import { SideBarData } from "./main";
+import {
+  AiOutlineClose,
+  AiFillSetting,
+  AiFillCalendar,
+  AiOutlineUserAdd,
+} from "react-icons/ai";
+import { BsFilm } from "react-icons/bs";
+import { useSelector } from "react-redux";
+
 function Navbar() {
   const [sidebar, setSideBar] = useState(false);
   const showSideBar = () => {
     setSideBar(!sidebar);
   };
+  const userProfile = useSelector((state) => {
+    return state.auth.profile;
+  });
+  const rederInfo = () => {
+    if (!userProfile) {
+      return (
+        <>
+          <NavLink activeClassName={style.active} to={"#"}>
+            Hi, {userProfile.hoTen}
+          </NavLink>
+          <NavLink to={"./"}>Log Out</NavLink>
+        </>
+      );
+    }
+  };
+  
+  const SideBarData = [
+    {
+      title: "List Film",
+      path: "/films",
+      icon: <BsFilm />,
+    },
+    {
+      title: "Add new film",
+      path: "/films/addnew",
+      icon: <BsFilm />,
+    },
+    
+    {
+      title: "User List",
+      path: "/users",
+      icon: <AiOutlineUserAdd />,
+    },
+    {
+      title: "Add User",
+      path: "/users/adduser",
+      icon: <AiOutlineUserAdd />,
+    },
+    
+    {
+      title: "Show Time",
+      path: "/showtime",
+      icon: <AiFillCalendar />,
+    },
+  ];
+
   return (
-    <>
+    <div>
       <div className={style.navbar}>
         <Link to={"#"} className={style.menubar}>
           <FaBars onClick={showSideBar} />
         </Link>
       </div>
-      <div className={sidebar ? `${style.navMenu} ${style.active}` : `${style.navMenu}`}>
+      <div
+        className={
+          sidebar ? `${style.navMenu} ${style.active}` : `${style.navMenu}`
+        }
+      >
         <ul className={style.navMenuItem}>
           <div className={style.navBarToggle}>
             <Link to={"#"} className={style.menubar} onClick={showSideBar}>
@@ -25,18 +82,17 @@ function Navbar() {
             {SideBarData.map((item, index) => {
               return (
                 <li key={index} className={style.navText}>
-                  <Link to={item.path}>
+                  <NavLink to={item.path} activeClassName={style.active} exact>
                     {item.icon}
                     <span>{item.title}</span>
-                  </Link>
+                  </NavLink>
                 </li>
               );
             })}
           </div>
         </ul>
       </div>
-      
-    </>
+    </div>
   );
 }
 
--- src/features/LogIn/pages/LogIn/index.jsx
+++ src/features/LogIn/pages/LogIn/index.jsx
@@ -1,74 +1,88 @@
-import { Button, Input } from "antd";
-import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
-import React, { useState } from "react";
-import style from "./style.module.css";
+import { Input, Button } from "antd";
 import { useFormik } from "formik";
+import React, { useState } from "react";
+import instance from "../../../../api/instance";
+import style from "./style.module.css";
 import * as yup from "yup";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+
 function LogIn() {
-  // const [isActive, setActive] = useState("false");
-  // const [isDisable, setDisable] = useState("true");
-  // const handleToggle = () => {
-  //   setActive(!isActive);
-  //   setDisable(!isDisable);
-  // };
-  const history = useHistory()
-  const goToFilms = () => {
-    history.push("/films");
+  const dispatch = useDispatch()
+  const schema = yup.object({
+    taiKhoan: yup.string().required("*Trường này bắt buộc nhập"),
+    matKhau: yup.string().required("*Trường này bắt buộc nhập"),
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      taiKhoan: "",
+      matKhau: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      const loginUser = { ...values};
+      signIn(values);
+    },
+    validationSchema: schema,
+  });
+  const signIn = async (user) => {
+    try {
+      setIsLoading(true);
+      const res = await instance.request({
+        url: "/api/QuanLyNguoiDung/DangNhap",
+        method: "POST",
+        data: user,
+      });
+      setIsLoading(false);
+      localStorage.setItem("token", res.data.content.accessToken)
+      const profile = {...res.data.content}
+      delete profile.accessToken;
+      console.log(res.data);
+      dispatch({
+        type:"auth/SET_PROFILE",
+        payload:profile,
+      })
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
-  // const schema = yup.object({
-  //   adminName: yup.string().required("*Trường này bắt buộc nhập"),
-  //   password: yup.string().required("*Trường này bắt buộc nhập"),
-  // });
-  // const formik = useFormik({
-  //   initialValues: {
-  //     adminName: "",
-  //     password: "",
-  //   },
-  //   onSubmit: (values) => {
-  //     console.log(values);
-  //     const loginUser = { ...values };
-  //     signIn(loginUser);
-  //   },
-  //   validationSchema: schema,
-  // });
-  // const signIn = async (values) => {
-  //   if(values.adminName==="hoangdat" && values.password==="3108") {
-  //     goToFilms()
-  //   }
-  // }
+  const history= useHistory()
+  const handleSubmit = () => {
+    history.push("/films")
+  }
   return (
-    <div className={style.background}>
-      <div className={style.container}>
-        {/* <Button onClick={handleToggle} className={`${!isDisable ? "disable" : ""}`}>Start</Button> */}
-        <form className={style.form} onSubmit={goToFilms}>
-          <h2>Login</h2>
-          <Input
-            className={style.input}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            size="large"
-            placeholder="Admin name"
-          />
-          {/* {formik.touched.adminName && formik.errors.adminName && (
-          <p className={style.error}>{formik.errors.adminName}</p>
-        )} */}
-          <Input.Password
-            className={style.input}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            placeholder="Password"
-            size="large"
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-          />
-          {/* {formik.touched.password && formik.errors.password && (
-          <p className={style.error}>{formik.errors.password}</p>
-        )} */}
-          <Button htmlType="submit" type="danger" >Enter</Button>
-        </form>
-      </div>
+    <div>
+      <h2 className={style.title}>SIGN IN</h2>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <Input
+          name="taiKhoan"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          className={style.input}
+          type="text"
+          placeholder="User Name"
+        />
+        {formik.touched.taiKhoan && formik.errors.taiKhoan && (
+          <p className={style.error}>{formik.errors.taiKhoan}</p>
+        )}
+        <Input
+          name="matKhau"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          className={style.input}
+          type="password"
+          placeholder="Password"
+        />
+        {formik.touched.matKhau && formik.errors.matKhau && (
+          <p className={style.error}>{formik.errors.matKhau}</p>
+        )}
+        <Button htmlType="submit" type="primary" loading={isLoading} >
+          Sign In
+        </Button>
+      </form>
     </div>
   );
 }
